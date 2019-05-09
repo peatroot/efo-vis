fdaf70a444a9cc29d56030e393036135f813d7f0
peatroot <garethpeat@gmail.com>
2019-05-09 20:58:49 +0100

    Add summary of protein coding genes

--- gene_parser.py
+++ gene_parser.py
@@ -2,22 +2,30 @@ import sys
 import json
 
 output_filename = 'genes.json'
+summary_output_filename = 'protein-coding-genes.json'
 
 def parse(filename):
     genes = {}
+    genes_summary = []
     with open(filename, 'r') as f_input:        
         for line in f_input:
             blob = json.loads(line)
             gene_id = blob['id']
             gene_symbol = blob['approved_symbol']
+            uniprot_id = blob['uniprot_id']
             genes[gene_symbol] = gene_id
-            # genes.append({
-            #     'id': gene_id,
-            #     'symbol': gene_symbol
-            # })
+            if blob['biotype'] == 'protein_coding':
+                genes_summary.append({
+                    'id': gene_id,
+                    'symbol': gene_symbol,
+                    'uniprotId': uniprot_id
+                })
 
     with open(output_filename, 'w') as f_output:
         json.dump(genes, f_output, indent=4, sort_keys=True)
+
+    with open(summary_output_filename, 'w') as f_output:
+        json.dump(genes_summary, f_output, indent=4, sort_keys=True)
 
 if __name__ == '__main__':
     filename = sys.argv[1]
